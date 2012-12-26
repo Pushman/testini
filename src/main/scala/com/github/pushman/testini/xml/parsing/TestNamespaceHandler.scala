d962a85781e95bb5f742151a197de099e3968934
Pushman <pushman.pl@gmail.com>
2012-12-26 16:03:44 +0100

    Poprawki konfiguracji scheme'y

--- src/main/scala/com/github/pushman/testini/xml/parsing/TestNamespaceHandler.scala
+++ src/main/scala/com/github/pushman/testini/xml/parsing/TestNamespaceHandler.scala
@@ -2,7 +2,7 @@ package com.github.pushman.testini.xml.parsing
 
 import org.springframework.beans.factory.xml.NamespaceHandlerSupport
 
-class TestDataNamespaceHandler extends NamespaceHandlerSupport {
+class TestNamespaceHandler extends NamespaceHandlerSupport {
 
   def init() {
     registerBeanDefinitionParser("case", new CaseBeanDefinitionParser)
